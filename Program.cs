5d13e5789dad2b6cf380f21e9fcb606d52de497b
alex.hatcher <alex.hatcher@slalom.com>
2021-02-09 00:33:52 +0000

    initial commit.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Amazon.S3;
+using Amazon.S3.Model;
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bucketName = Guid.NewGuid().ToString().ToLower();      
+            var creds = new Amazon.Runtime.BasicAWSCredentials("key", "secret");
+            var s3Client = new AmazonS3Client(creds, new AmazonS3Config { ServiceURL = Environment.GetEnvironmentVariable("AWS_S3_ENDPOINT"), ForcePathStyle = true });
+            //s3Client.ListBucketsAsync().Wait();
+            s3Client.PutBucketAsync(new PutBucketRequest { BucketName = bucketName, UseClientRegion = true }).Wait();
+            Console.WriteLine("Bucket Created!");
+            // visit to see results http://localhost:9090
+        }
+    }
+}
